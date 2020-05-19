2256375b9c241822561716e7e70193d75eb8c88c
Grzegorz Olszewski <grzegorz@olszewski.in>
2020-05-19 04:39:29 +0200

    Initial index page

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SuSi Monitor - easy to set-up, powerful web service response monitor</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.html"></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <!--<li class="nav-item">
+            <a class="nav-link" href="features.php">Features</a>
+          </li>-->
+          <li class="nav-item">
+            <a class="nav-link" href="https://github.com/susi-monitor/docs" target="_blank">Documentation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="download.php">Download</a>
+          </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/susi-monitor/susi-monitor" target="_blank">Github</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <!-- Slide One - Set the background image for this slide in the line below -->
+        <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+        <!--  <div class="carousel-caption d-none d-md-block">
+            <h3>First Slide</h3>
+            <p>This is a description for the first slide.</p>
+          </div>-->
+        </div>
+      </div>
+      <!--<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>-->
+    </div>
+  </header>
+
+  <!-- Page Content -->
+  <div class="container">
+<br>
+      <!-- Features Section -->
+      <div class="row">
+          <div class="col-lg-6">
+              <h2>SuSi Monitor Features</h2>
+              <br>
+              <ul>
+                  <li><strong>Enterprise-friendly</strong> - system set-up behind a corporate firewall?<br><i>No problem.</i><br>
+                      Proxies with authorization needed to access your services? <br><i>We got it</i>.
+                  </li>
+                  <li>
+                      Minimal system requirements - SuSi will work at pretty much any machine, be it Xeon-Powered server
+                      cluster
+                      or Raspberry-Pi
+                  </li>
+                  <li>SUper SImple - as straightforward as it can possibly be (and we're working to make it even
+                      better)
+                  </li>
+                  <li>Notifications - receive system downtime notifications straight to Microsoft Teams</li>
+              </ul>
+          </div>
+          <div class="col-lg-6">
+              <img class="img-fluid rounded" src="http://placehold.it/700x450" alt="">
+          </div>
+      </div>
+      <!-- /.row -->
+
+    <hr>
+
+    <!-- Call to Action Section -->
+    <div class="row mb-4">
+      <div class="col-md-8">
+        <p>Why trust descriptions? Get it now and see for yourself - installation is quick and simple. </p>
+      </div>
+      <div class="col-md-4">
+        <a class="btn btn-lg btn-secondary btn-block" href="download.php">Get it now</a>
+      </div>
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy;<?php echo date('Y'); ?> SuSi Monitor</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
